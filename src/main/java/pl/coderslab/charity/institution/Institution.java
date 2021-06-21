52f75b71d99f9d264834b4ec6dc8f715ed92de73
kubala2908 <j.b.tuminski@gmail.com>
2021-06-21 22:52:11 +0200

    first commit

---
+++ src/main/java/pl/coderslab/charity/institution/Institution.java
@@ -0,0 +1,22 @@
+package pl.coderslab.charity.institution;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "institution")
+public class Institution {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+}
